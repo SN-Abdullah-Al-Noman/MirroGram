19fa092ebd664bf4ff7c07e7a9f41b486256bee0
Abdullah Al Noman <125588871+SN-Abdullah-Al-Noman@users.noreply.github.com>
2024-06-09 12:40:50 +0600

    Update bot_commands.py

--- bot/helper/telegram_helper/bot_commands.py
+++ bot/helper/telegram_helper/bot_commands.py
@@ -4,20 +4,20 @@ from bot import CMD_SUFFIX
 class _BotCommands:
     def __init__(self):
         self.StartCommand = f"start{CMD_SUFFIX}"
-        self.MirrorCommand = [f"mirror{CMD_SUFFIX}", f"m{CMD_SUFFIX}"]
-        self.QbMirrorCommand = [f"qbmirror{CMD_SUFFIX}", f"qm{CMD_SUFFIX}"]
-        self.JdMirrorCommand = [f"jdmirror{CMD_SUFFIX}", f"jm{CMD_SUFFIX}"]
-        self.YtdlCommand = [f"ytdl{CMD_SUFFIX}", f"y{CMD_SUFFIX}"]
-        self.LeechCommand = [f"leech{CMD_SUFFIX}", f"l{CMD_SUFFIX}"]
-        self.QbLeechCommand = [f"qbleech{CMD_SUFFIX}", f"ql{CMD_SUFFIX}"]
-        self.JdLeechCommand = [f"jdLeech{CMD_SUFFIX}", f"jl{CMD_SUFFIX}"]
-        self.YtdlLeechCommand = [f"ytdlleech{CMD_SUFFIX}", f"yl{CMD_SUFFIX}"]
+        self.MirrorCommand = f"mirror{CMD_SUFFIX}"
+        self.QbMirrorCommand = f"qbmirror{CMD_SUFFIX}"
+        self.JdMirrorCommand = f"jdmirror{CMD_SUFFIX}"
+        self.YtdlCommand = f"ytdl{CMD_SUFFIX}"
+        self.LeechCommand = f"leech{CMD_SUFFIX}"
+        self.QbLeechCommand = f"qbleech{CMD_SUFFIX}"
+        self.JdLeechCommand = f"jdLeech{CMD_SUFFIX}"
+        self.YtdlLeechCommand = f"ytdlleech{CMD_SUFFIX}"
         self.CloneCommand = f"clone{CMD_SUFFIX}"
         self.CountCommand = f"count{CMD_SUFFIX}"
         self.DeleteCommand = f"del{CMD_SUFFIX}"
-        self.CancelTaskCommand = [f"cancel{CMD_SUFFIX}", f"c{CMD_SUFFIX}"]
+        self.CancelTaskCommand = f"c{CMD_SUFFIX}"
         self.CancelAllCommand = f"cancelall{CMD_SUFFIX}"
-        self.ForceStartCommand = [f"forcestart{CMD_SUFFIX}", f"fs{CMD_SUFFIX}"]
+        self.ForceStartCommand = f"forcestart{CMD_SUFFIX}"
         self.ListCommand = f"list{CMD_SUFFIX}"
         self.SearchCommand = f"search{CMD_SUFFIX}"
         self.StatusCommand = f"status{CMD_SUFFIX}"
@@ -35,11 +35,11 @@ class _BotCommands:
         self.AExecCommand = f"aexec{CMD_SUFFIX}"
         self.ExecCommand = f"exec{CMD_SUFFIX}"
         self.ClearLocalsCommand = f"clearlocals{CMD_SUFFIX}"
-        self.BotSetCommand = [f"bsetting{CMD_SUFFIX}", f"bs{CMD_SUFFIX}"]
-        self.UserSetCommand = [f"usetting{CMD_SUFFIX}", f"us{CMD_SUFFIX}"]
+        self.BotSetCommand = f"bsetting{CMD_SUFFIX}"
+        self.UserSetCommand = f"usetting{CMD_SUFFIX}"
         self.BtSelectCommand = f"btsel{CMD_SUFFIX}"
         self.RssCommand = f"rss{CMD_SUFFIX}"
-        self.CancelMirror = f"c{CMD_SUFFIX}"
         self.BroadcastCommand = f"broadcast{CMD_SUFFIX}"
+        self.MediaInfoCommand = f"mediainfo{CMD_SUFFIX}"
 
 BotCommands = _BotCommands()
